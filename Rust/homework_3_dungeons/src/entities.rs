f1bc9b0421e38f93764dadd14a39c2c409f1dcb6
Petko <petko.krasimirov99@gmail.com>
2021-12-27 23:06:43 +0200

    Add first part of third rust homework

--- Rust/homework_3_dungeons/src/entities.rs
+++ Rust/homework_3_dungeons/src/entities.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, rc::Rc, ops::{DerefMut, RangeBounds}};
+
 
 /// Различните грешки, които ще очакваме да върнете като резултат от някои невалидни операции.
 /// Повече детайли по-долу.
@@ -15,7 +16,7 @@ pub enum Errors {
 /// Четирите посоки, в които може една стая да има съседи. Може да добавите още trait
 /// имплементации, за да си улесните живота.
 ///
-#[derive(Debug,Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Hash, Eq)]
 pub enum Direction {
     North,
     South,
@@ -23,18 +24,30 @@ pub enum Direction {
     West,
 }
 
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::East => Direction::West,
+            Direction::West => Direction::East,
+        }
+    }
+}
+
 /// Една стая в подземията. Дефинира се само с име, макар че в по-интересна имплементация може да
 /// държи item-и, противници...
 ///
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Room {
     pub name: String,
-    // Каквито други полета ви трябват
+    pub connections: HashMap<Direction, String>,
 }
 
 /// Контейнер за стаите и не само. Ще работим предимно със тази структура.
 ///
 pub struct Dungeon {
-    pub rooms: HashMap<String, Room>,
+    pub rooms: HashMap<String, Rc<Room>>,
 }
 
 impl Dungeon {
@@ -53,7 +66,13 @@ impl Dungeon {
         if self.rooms.contains_key(name) {
             Err(Errors::DuplicateRoom(name.to_string()))
         } else {
-            self.rooms.insert(name.to_string(), Room { name: name.to_string() });
+            self.rooms.insert(
+                name.to_string(),
+                Rc::new(Room {
+                    name: name.to_string(),
+                    connections: HashMap::new(),
+                }),
+            );
             Ok(())
         }
     }
@@ -64,7 +83,11 @@ impl Dungeon {
     /// Ако няма такава стая, очакваме `Errors::UnknownRoom` с подаденото име.
     ///
     pub fn get_room(&self, room_name: &str) -> Result<&Room, Errors> {
-        todo!()
+        if let Some(room) = self.rooms.get(room_name) {
+            Ok(room)
+        } else {
+            Err(Errors::UnknownRoom(room_name.to_string()))
+        }
     }
 
     /// Добавяне на съсед на дадена стая. След извикването на функцията, очакваме стаята с име
@@ -77,12 +100,24 @@ impl Dungeon {
     /// върнете тази, която проверявате първо.
     ///
     pub fn set_link(
-        &mut self,
+        & mut self,
         room_name: &str,
         direction: Direction,
         other_room_name: &str,
     ) -> Result<(), Errors> {
-        todo!()
+        if !self.rooms.contains_key(room_name) {
+            return Err(Errors::UnknownRoom(room_name.to_string()));
+        }
+        if !self.rooms.contains_key(other_room_name) {
+            return Err(Errors::UnknownRoom(other_room_name.to_string()));
+        }
+        let room = Rc::get_mut(self.rooms.get_mut(room_name).unwrap()).unwrap();
+        room.connections.insert(direction, other_room_name.to_string());
+
+        let other_room = Rc::get_mut(self.rooms.get_mut(other_room_name).unwrap()).unwrap();
+        other_room.connections.insert(direction.opposite(), room_name.to_string());
+
+        Ok(())
     }
 
     /// Четене на съседа на стаята с име `room_name` в посока `direction`. Тук има няколко
@@ -92,7 +127,19 @@ impl Dungeon {
     /// - Ако подадената стая няма съсед в тази посока, Ok(None) е смисления резултат
     /// - Иначе, чакаме reference към `Room` структурата на въпросния съсед, опакована в `Ok(Some(`.
     ///
-    pub fn get_next_room(&self, room_name: &str, direction: Direction) -> Result<Option<&Room>, Errors> {
-        todo!()
+    pub fn get_next_room(
+        &self,
+        room_name: &str,
+        direction: Direction,
+    ) -> Result<Option<&Room>, Errors> {
+        if !self.rooms.contains_key(room_name) {
+            return Err(Errors::UnknownRoom(room_name.to_string()));
+        }
+        let room = self.rooms.get(room_name).unwrap();
+        if let Some(next_room_name) = room.connections.get(&direction) {
+            Ok(Some(self.rooms.get(next_room_name).unwrap()))
+        } else {
+            Ok(None)
+        }
     }
 }
--- Rust/homework_3_dungeons/tests/test_dungeon.rs
+++ Rust/homework_3_dungeons/tests/test_dungeon.rs
@@ -1,4 +1,4 @@
-use homework_3_dungeons::entities::Dungeon;
+use homework_3_dungeons::entities::{Direction, Dungeon};
 
 #[test]
 fn create_empty_dungeon() {
@@ -16,10 +16,87 @@ fn add_room_to_empty_dungeon() {
 }
 
 #[test]
-fn duplicate_room (){
+fn duplicate_room() {
     let mut dungeon = Dungeon::new();
     dungeon.add_room("Room 1").unwrap();
     assert_eq!(dungeon.rooms.len(), 1);
     dungeon.add_room("Room 1").unwrap_err();
     assert_eq!(dungeon.rooms.len(), 1);
+}
+
+#[test]
+fn add_connection_and_get_room() {
+    let mut dungeon = Dungeon::new();
+
+    dungeon.add_room("Entrance").unwrap();
+    dungeon.add_room("Hallway").unwrap();
+    dungeon
+        .set_link("Entrance", Direction::East, "Hallway")
+        .unwrap();
+
+    assert_eq!(dungeon.get_room("Hallway").unwrap().name, "Hallway");
+    assert_eq!(
+        dungeon
+            .get_next_room("Hallway", Direction::West)
+            .unwrap()
+            .unwrap()
+            .name,
+        "Entrance"
+    );
+}
+
+#[test]
+fn override_connection() {
+    let mut dungeon = Dungeon::new();
+
+    dungeon.add_room("Entrance").unwrap();
+    dungeon.add_room("Hallway").unwrap();
+    dungeon.add_room("Magic Lab").unwrap();
+
+    dungeon
+        .set_link("Entrance", Direction::East, "Hallway")
+        .unwrap();
+    dungeon
+        .set_link("Hallway", Direction::West, "Magic Lab")
+        .unwrap();
+
+    assert_eq!(
+        dungeon
+            .get_next_room("Entrance", Direction::East)
+            .unwrap()
+            .unwrap()
+            .name,
+        "Hallway"
+    );
+    assert_eq!(
+        dungeon
+            .get_next_room("Hallway", Direction::West)
+            .unwrap()
+            .unwrap()
+            .name,
+        "Magic Lab"
+    );
+}
+
+#[test]
+fn get_room_not_found() {
+    let mut dungeon = Dungeon::new();
+    dungeon.add_room("Entrance").unwrap();
+    dungeon.add_room("Hallway").unwrap();
+    dungeon
+        .set_link("Entrance", Direction::East, "Hallway")
+        .unwrap();
+
+    assert_eq!(dungeon.get_next_room("Entrance", Direction::West).unwrap(), None);
+}
+
+#[test]
+fn concet_one_room_with_self() {
+    let mut dungeon = Dungeon::new();
+    dungeon.add_room("Entrance").unwrap();
+    dungeon
+        .set_link("Entrance", Direction::East, "Entrance").unwrap();
+    assert_eq!(dungeon.rooms.len(), 1);
+    assert_eq!(dungeon.get_next_room("Entrance", Direction::East).unwrap().unwrap().name, "Entrance");
+    assert_eq!(dungeon.get_next_room("Entrance", Direction::West).unwrap().unwrap().name, "Entrance");
 }
